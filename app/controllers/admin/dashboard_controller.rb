891f26967a4b7865bf9385b7c9c7adb9d357fa1b
Nightowsky <96236612+Nightowsky@users.noreply.github.com>
2022-03-03 15:01:12 +0100

    Dashboard specific layout set

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 module Admin 
-    class DashboardController < ApplicationController
    
+    class DashboardController < ApplicationController
+    before_action :authenticate_admin_user!
+    layout 'admin'   
+
     def dashboard
-    
     end
 
     def index
-
     end
-
   end
 end
 
